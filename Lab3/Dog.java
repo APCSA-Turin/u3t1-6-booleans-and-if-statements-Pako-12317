4802bd372a7ed7e30b27881d1a879ada48287656
Pako-12317 <patrykm10@nycstudents.net>
2024-10-23 21:24:32 +0000

    ,commit

---
+++ Lab3/Dog.java
@@ -0,0 +1,12 @@
+
+public class Dog {
+String name;
+int age;
+boolean bool;
+    public Dog(String string, int i, boolean b) {
+        this.name=string;
+        this.age=i;
+        this.bool=b;
+    }
+
+}
